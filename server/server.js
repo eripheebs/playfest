e48cf3ce6bb0f7ba5b3fb40a527a7c119a112e58
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-05-04 17:48:09 +0100

    started passport implementation

--- server/server.js
+++ server/server.js
@@ -2,12 +2,34 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
+var passport = require('passport');
+var SpotifyStrategy = require('passport-spotify').Strategy;
+
 var sendToClientRoutes = require('./routes/sendToClient.js');
 
+var client_id     = "873379a32a354918b0a54f4f79736716";
+var client_secret = "433083b384654bbb84ea7a1712470313";
+
+app.use(passport.initialize());
 app.use('/', sendToClientRoutes);
+
+app.get('/auth/new', function(req, res){
+ console.log('Jazzy is the cutest!');
+ res.sendStatus(200);
+});
+
 
 app.listen(port, function(){
   console.log('Your Playfest Backend is running on port ' + port);
 });
+
+passport.use(new SpotifyStrategy ({
+  clientID: client_id,
+  clientSecret: client_secret,
+  callbackURL: "http://localhost:5000/auth/callback"
+},
+function(accessToken, refreshToken, spotifyProfile, done) {
+  return done(null, spotifyProfile);
+}));
 
 module.exports = app;
---
+++ test/backend/authenticationSpec.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../server/server.js');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Spotify Authentication', function(){
+
+  describe('when authentication request is made', function(){
+
+    it('user is redirected to the Spotify auth', function(done){
+      chai.request(server)
+        .get('/auth/new')
+        .end(function(err, res){
+          expect(res.status).to.equal(200);
+          done();
+        });
+
+    });
+  });
+
+});

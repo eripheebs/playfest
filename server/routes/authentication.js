6cf42f7d77b20adc62a9325e1231297a2a3516c7
Robota <lorenzo.turrino@gmail.com>
2016-05-04 18:59:43 +0100

    switch return function to extract AuthToken instead of user profile, + some cleanup

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -5,7 +5,8 @@ var passport = require('passport');
 router.get('/new', passport.authenticate('spotify'));
 
 router.get('/callback', passport.authenticate('spotify', { session: false }), function(req, res){
-  res.send(200);
+  console.log(req.user);
+  res.redirect('/');
 });
 
 module.exports = router;

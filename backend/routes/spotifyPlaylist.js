21cede114850eef75b9308baf531f86937a32b16
eripheebs <erikapheby@gmail.com>
2016-05-10 15:59:25 +0100

    fixed edge case which tells  you you must be logged in to make playlist

--- backend/routes/spotifyPlaylist.js
+++ backend/routes/spotifyPlaylist.js
@@ -3,13 +3,20 @@ var router = express.Router();
 
 var getTracksAndCreatePlaylist = require('../models/spotifyPlaylist.js').getTracksAndCreatePlaylist;
 
+var loginError = "You must be logged in";
+
 router.post('/new', function(req, res){
   var arrayOfArtists = req.body.arrayOfArtists;
   var playlistName = req.body.playlistName;
+  if (_userNotLoggedIn(req.user)) { res.send(loginError); };
   getTracksAndCreatePlaylist(arrayOfArtists, req.user.userId, req.user.accessToken, playlistName)
     .then(function(confData){
       res.send(confData);
     })
 });
+
+_userNotLoggedIn = function(user){
+  return (user == undefined);
+}
 
 module.exports = router;

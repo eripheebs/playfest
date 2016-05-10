11a1a0fe2159b65952a51f13841e4fbc0b6453bd
eripheebs <erikapheby@gmail.com>
2016-05-10 18:53:53 +0100

    sendding picture to backend

--- frontend/www/js/controllers/pictureController.js
+++ frontend/www/js/controllers/pictureController.js
@@ -1,7 +1,7 @@
 playfestApp.controller('pictureCtrl', function($scope, $cordovaCamera) {
 
   $scope.takePicture = function() {
-    
+
           var options = {
               quality : 75,
               destinationType : Camera.DestinationType.DATA_URL,
@@ -16,6 +16,15 @@ playfestApp.controller('pictureCtrl', function($scope, $cordovaCamera) {
 
     $cordovaCamera.getPicture(options).then(function(imageData) {
       $scope.imgURI = "data:image/jpeg;base64," + imageData;
+      $upload.upload({
+               url: 'http://localhost:5000/poster',
+               method: 'POST',
+               file: imageData,
+           }).progress(function(evt) {
+               console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+           }).success(function(data, status, headers, config) {
+               console.log("Image uploaded!");
+           });
     }, function(err) {
       console.log("It isn't working, mate.")
     });

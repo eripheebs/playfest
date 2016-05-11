86c8d9c7199a93e0f762286634c3033330c6ce0d
eripheebs <erikapheby@gmail.com>
2016-05-11 18:33:23 +0100

    button that redirects to spotify

---
+++ frontend/www/js/controllers/goToSpotifyController.js
@@ -0,0 +1,30 @@
+playfestApp.controller('goToSpotifyController', function($scope, $cordovaInAppBrowser) {
+
+  document.addEventListener("deviceready", onDeviceReady, false);
+
+  function onDeviceReady() {
+    console.log(device.cordova);
+  };
+
+  $scope.launchSpotify = function(){
+    var scheme;
+    console.log(device.platform);
+
+    if(device.platform === 'iOS') {
+        scheme = 'spotify://';
+    } else if(device.platform === 'Android') {
+        scheme = 'com.spotify.android';
+    };
+
+    appAvailability.check(
+      scheme,
+      function() {
+          window.open('spotify://', '_system', 'location=no');
+          console.log('Twitter is available');
+      },
+      function() {
+          window.open('https://spotify.com/', '_system', 'location=no');
+          console.log('Twitter is not available');
+      });
+  };
+});
